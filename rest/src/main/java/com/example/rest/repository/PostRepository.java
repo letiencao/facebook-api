965b91ca4ad2b4073986e6538d5e30cfe3119268
Le Tien Cao <caole28072000@gmail.com>
2022-10-31 21:47:23 +0700

    Update Code

--- rest/src/main/java/com/example/rest/repository/PostRepository.java
+++ rest/src/main/java/com/example/rest/repository/PostRepository.java
@@ -2,10 +2,12 @@ package com.example.rest.repository;
 
 import com.example.rest.model.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post,Integer> {
+    @Query(value = "SELECT * FROM post WHERE post.id = :id AND user.deleted = false", nativeQuery = true)
     Post findById(int id);
 
     Post save(Post post);
